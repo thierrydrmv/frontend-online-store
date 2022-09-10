776b0051ebffaa6b6d0237035a0417f79afa9737
Thierry <varelathierry@gmail.com>
2022-09-10 16:49:16 -0300

    requisito 10

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,24 +5,80 @@ export default class Cart extends Component {
     produtosNoCarrinho: [],
   };
 
-  componentDidMount() {
-    const produtosSalvos = JSON.parse(localStorage.getItem('produtosSalvos'));
-    this.setState({ produtosNoCarrinho: produtosSalvos });
+  async componentDidMount() {
+    const produtosSalvos = await JSON.parse(localStorage.getItem('produtosSalvos'));
+    this.setState({
+      produtosNoCarrinho: produtosSalvos,
+    });
   }
+
+  increaseQuantity = ({ target }) => {
+    const { produtosNoCarrinho } = this.state;
+    const { firstChild } = target.parentNode;
+    const titleProduct = (
+      firstChild.innerText);
+    const filter = produtosNoCarrinho.filter(({ title }) => title === titleProduct);
+    filter[0].quantidade += 1;
+    this.setState({ produtosNoCarrinho });
+    localStorage.setItem('produtosSalvos', JSON.stringify(produtosNoCarrinho));
+  };
+
+  decreaseQuantity = ({ target }) => {
+    const { produtosNoCarrinho } = this.state;
+    const { firstChild } = target.parentNode;
+    const titleProduct = (
+      firstChild.innerText);
+    const filter = produtosNoCarrinho.filter(({ title }) => title === titleProduct);
+    if (filter[0].quantidade > 1) {
+      filter[0].quantidade -= 1;
+      this.setState({ produtosNoCarrinho });
+      localStorage.setItem('produtosSalvos', JSON.stringify(produtosNoCarrinho));
+    }
+  };
+
+  removeFromCart = ({ target }) => {
+    const { produtosNoCarrinho } = this.state;
+    const { firstChild } = target.parentNode;
+    const titleProduct = (
+      firstChild.innerText);
+    const filter = produtosNoCarrinho.filter(({ title }) => title !== titleProduct);
+    this.setState({ produtosNoCarrinho: filter });
+    localStorage.setItem('produtosSalvos', JSON.stringify(filter));
+  };
 
   render() {
     const { produtosNoCarrinho } = this.state;
     return (
       <div>
-        { console.log(produtosNoCarrinho) }
         {
-          produtosNoCarrinho ? (
+          produtosNoCarrinho && produtosNoCarrinho.length ? (
             produtosNoCarrinho.map(({ title, price, image, quantidade }) => (
               <div key={ title }>
                 <p data-testid="shopping-cart-product-name">{ title }</p>
                 <p>{ price }</p>
                 <img src={ image } alt={ title } />
                 <p data-testid="shopping-cart-product-quantity">{ quantidade }</p>
+                <button
+                  onClick={ this.increaseQuantity }
+                  data-testid="product-increase-quantity"
+                  type="button"
+                >
+                  +
+                </button>
+                <button
+                  onClick={ this.decreaseQuantity }
+                  data-testid="product-decrease-quantity"
+                  type="button"
+                >
+                  -
+                </button>
+                <button
+                  onClick={ this.removeFromCart }
+                  data-testid="remove-product"
+                  type="button"
+                >
+                  X
+                </button>
               </div>
             ))
           ) : (
@@ -31,9 +87,7 @@ export default class Cart extends Component {
             </div>
           )
         }
-
       </div>
-
     );
   }
 }
